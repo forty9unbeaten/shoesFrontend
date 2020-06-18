fa7e5b89b1929aa44307e9e04a21a194b4bbb6e6
Rob Spears <rob.sp3ars@gmail.com>
2020-06-18 17:57:00 -0400

    Render component appropriately

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            shoes: [],
+        };
+    }
+
+    componentDidMount() {
+        fetch('http://127.0.0.1:8000/api/shoes', {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                this.setState({ shoes: data });
+            });
+    }
+
+    renderShoeInfo() {
+        const { shoes } = this.state;
+        const colors = {
+            WHT: 'White',
+            BLK: 'Black',
+            VLT: 'Violet',
+            IND: 'Indigo',
+            BLU: 'Blue',
+            GRN: 'Green',
+            YLW: 'Yellow',
+            ORG: 'Orange',
+            RED: 'Red',
+        };
+        return shoes.map((shoe) => (
+            <div className="shoe" key={shoes.indexOf(shoe)}>
+                <strong>{`${shoe.manufacturer} - ${shoe.brand_name}`}</strong>
+                <ul>
+                    <li>Size --> {shoe.size}</li>
+                    <li>Material --> {shoe.material}</li>
+                    <li>Fasten Type --> {shoe.fasten_type}</li>
+                    <li>Color --> {colors[shoe.color]}</li>
+                    <li>Type --> {shoe.shoe_type}</li>
+                </ul>
+            </div>
+        ));
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h1>The Shoes</h1>
+                <h4>List of shoes in the database...</h4>
+                {this.state.shoes ? this.renderShoeInfo() : `Loading...`}
+            </div>
+        );
+    }
 }
 
 export default App;
